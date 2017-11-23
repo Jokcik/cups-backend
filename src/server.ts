08642d35371d8dccfa0fee2194a81e77edab515e
jokci <jokcik@yandex.ru>
2017-11-23 12:18:51 +0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 import {HttpExceptionFilter} from './modules/exception/http-exception.filter';
+import * as cors from 'express-cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.use(bodyParser.json());
   app.setGlobalPrefix('api');
+  app.use(cors({allowedOrigins: ['localhost:3000']}));
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3001);
 }
